815b879b565e6d6387d5ec7e7fb1396223b5f8de
Lumacadlbl <lumacadlbl@gmail.com>
2025-08-19 23:03:50 -0700

    hahay

---
+++ src/prelimlumacad/Prelimlumacad.java
@@ -0,0 +1,16 @@
+
+package prelimlumacad;
+
+import java.util.Scanner;
+
+public class Prelimlumacad {
+
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter price of Item: ");
+        int item = sc.nextInt();
+    
+}
+}
